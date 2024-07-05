5ec276abdbbdc4e17949eb5d695edc3fea1a96ef
Aftab_vip_king <168022130+Aftab637s@users.noreply.github.com>
2024-07-05 21:03:09 +0530

    Update couples.py

--- ANNIEMUSIC/plugins/tools/couples.py
+++ ANNIEMUSIC/plugins/tools/couples.py
@@ -39,7 +39,7 @@ async def ctest(_, message):
     try:
      #  is_selected = await get_couple(cid, today)
      #  if not is_selected:
-         msg = await message.reply_text("🦋")
+         msg = await message.reply_text("😏")
          #GET LIST OF USERS
          list_of_users = []
 
